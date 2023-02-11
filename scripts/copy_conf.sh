753c3e6a5a4d65aa3b797279a68c6a07b40195e9
ivan <isnob46@gmail.com>
2023-02-11 12:03:57 +0100

    add webrtc experiment

--- scripts/copy_conf.sh
+++ scripts/copy_conf.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sudo cp ./nginx.hass.subdomain.conf ./swag-data/nginx/proxy-confs/hass.subdomain.conf
 sudo cp ./nginx.grafana.subdomain.conf ./swag-data/nginx/proxy-confs/grafana.subdomain.conf
+sudo cp ./nginx.webrtc.subdomain.conf ./swag-data/nginx/proxy-confs/nginx.webrtc.subdomain.conf
+
 sudo cp ./hass.configuration.yml ./hass-data/configuration.yaml
+sudo cp ./template.yaml ./hass-data/template.yaml
 sudo cp ./mosquitto.conf ./mosquitto/config/mosquitto.conf
